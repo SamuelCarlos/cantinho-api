de9292ead3e5ba11505fd1d857564c20b530a885
SamuelCarlos <samuel_cxc@hotmail.com>
2021-10-05 04:05:21 -0300

    chore: scripts

---
+++ src/typings/index.d.ts
@@ -0,0 +1,6 @@
+declare module '@types/express';
+declare module '@types/qrcode';
+declare module '@types/uuid';
+declare module '@types/cors';
+declare module '@types/bcrypt';
+declare module '@types/jsonwebtoken';
