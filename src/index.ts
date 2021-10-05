e5a50a2ad004f191492b5f3a57519e53a001070c
SamuelCarlos <samuel_cxc@hotmail.com>
2021-10-05 04:32:56 -0300

    chore: scripts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
-require('dotenv').config();
 require('./config/aws');
 
 import router from './routes';
@@ -13,6 +14,8 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(router);
 
