74b5508b5f11fad6814c6de58158e19008d477a4
SamuelCarlos <samuel_cxc@hotmail.com>
2021-10-05 02:10:11 -0300

    feat: deploy

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(router);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('server is listening on port 3000');
 });
 
